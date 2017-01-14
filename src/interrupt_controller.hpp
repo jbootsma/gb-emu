be7dd733a241b114de7f871db8af6e466a1c48e4
James Bootsma <jrbootsma@gmail.com>
2017-01-14 13:30:59 -0700

    Clean up interrupt_controller
    Clean up the code to be more consistent with style of the rest of the project.

--- src/interrupt_controller.hpp
+++ src/interrupt_controller.hpp
@@ -3,29 +3,31 @@
 
 #include <cstdint>
 
-static const std::uint16_t IF_ADR = 0xFF0F;
-static const std::uint16_t IE_ADR = 0xFFFF;
+static const std::uint16_t IF_ADR = 0xff0f;
+static const std::uint16_t IE_ADR = 0xffff;
 
 class InterruptController
 {
-private:
-    std::uint8_t IE = 0;
-    std::uint8_t IF = 0;
-
 public:
-    bool interrupt_pending() { return !!(IE & IF); }
+    bool interrupt_pending() const { return !!(IE & IF); }
     std::uint8_t accept_interrupt();
 
-    void setIF(std::uint8_t val) { IF = val & 0x1f; }
-    void setIE(std::uint8_t val) { IE = val & 0x1f; }
-    std::uint8_t getIF() { return IF | 0xe0; }
-    std::uint8_t getIE() { return IE; }
+    void setIF(std::uint8_t val) { IF = val & ~unused; }
+    void setIE(std::uint8_t val) { IE = val & ~unused; }
+    std::uint8_t getIF() const { return IF | unused; }
+    std::uint8_t getIE() const { return IE; }
 
     void signal_v_blank_irq() { IF |= 0x01; }
     void signal_lcd_stat_irq() { IF |= 0x02; }
     void signal_timer_irq() { IF |= 0x04; }
     void signal_serial_irq() { IF |= 0x08; }
     void signal_joypad_irq() { IF |= 0x10; }
+
+private:
+    static const std::uint8_t unused = 0xe0;
+
+    std::uint8_t IE = 0;
+    std::uint8_t IF = 0;
 };
 
 #endif
